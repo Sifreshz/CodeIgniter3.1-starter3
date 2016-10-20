419669c5c1442315beedeeee276956ffe868a0d1
Injo Baik <baikinjo.28@gmail.com>
2016-10-20 15:59:55 -0700

    Complted Part 3 of Lab 5 - added shopping controller and view models
    Signed-off-by: Injo Baik <baikinjo.28@gmail.com>

---
+++ application/controllers/Shopping.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Baik
+ * Date: 10/20/2016
+ * Time: 3:51 PM
+ */
+class Shopping extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        $stuff = file_get_contents('../data/receipt.md');
+        $this->data['receipt'] = $this->parsedown->parse($stuff);
+        $this->data['content'] = '';
+        // pictorial menu
+        $count = 1;
+        foreach ($this->categories->all() as $category) {
+            $chunk = 'category' . $count;
+            $this->data[$chunk] = $this->parser->parse('category-shop',$category,true);
+            foreach($this->menu->all() as $menuitem) {
+                if ($menuitem->category != $category->id) continue;
+                $this->data[$chunk] .= $this->parser->parse('menuitem-shop',$menuitem,true);
+            }
+            $count++;
+        }
+        $this->render('template-shopping');
+
+    }
+
+}
